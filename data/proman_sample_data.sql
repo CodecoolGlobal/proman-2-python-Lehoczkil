bf6bb7d4a5e4ab5bc233560499f3dde5180e8e51
mllorand <m.lorandlevente@gmail.com>
2022-05-19 19:19:51 +0200

    Fix loadCards, (database schema fix)

--- data/proman_sample_data.sql
+++ data/proman_sample_data.sql
@@ -67,12 +67,12 @@ INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 2, 'in progress card', 1);
 INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 3, 'planning', 1);
 INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 4, 'done card 2', 2);
 INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 4, 'done card 1', 1);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 1, 'NEW CARD 2', 2);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 1, 'NEW CARD 1', 1);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 2, 'IN PROGRESS CARD', 1);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 3, 'PLANNING', 1);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 4, 'DONE CARD 2', 2);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 4, 'DONE CARD 1', 1);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 5, 'NEW CARD 2', 2);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 5, 'NEW CARD 1', 1);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 6, 'IN PROGRESS CARD', 1);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 7, 'PLANNING', 1);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 8, 'DONE CARD 2', 2);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 8, 'DONE CARD 1', 1);
 
 ---
 --- add constraints
