43b5a910ce259f7095a898629ae7293acbe30942
Daniel Fekete <danielt.fekete@gmail.com>
2022-06-01 22:01:05 +0200

    Fix drag and drop function on frontend
    -prevent drop cards to another board than parent
    -implement rearrange card position

--- static/js/controller/boardsManager.js
+++ static/js/controller/boardsManager.js
@@ -1,7 +1,13 @@
 import {dataHandler} from "../data/dataHandler.js";
-import {addCardButtonBuilder, boardBuilder, columnsBuilder, newColumnBuilder, cardBuilder} from "../view/htmlFactory.js";
+import {
+    addCardButtonBuilder,
+    boardBuilder,
+    cardBuilder,
+    columnsBuilder,
+    newColumnBuilder
+} from "../view/htmlFactory.js";
 import {domManager} from "../view/domManager.js";
-import {cardsManager, deleteCardButtonHandler, deleteBoardButtonHandler} from "./cardsManager.js";
+import {cardsManager, deleteBoardButtonHandler, deleteCardButtonHandler} from "./cardsManager.js";
 
 export let boardsManager = {
     loadBoards: async function () {
@@ -14,8 +20,8 @@ export let boardsManager = {
             addColumnButton.addEventListener("click", addColumnHandler);
             const showHideButton = document.querySelector(`.toggle-board-button[data-board-id="${board.id}"]`);
             const deleteBoardButton = document.querySelector(`.board-remove[data-board-id-remove="${board.id}"]`);
-            deleteBoardButton.addEventListener("click",deleteBoardButtonHandler)
-            showHideButton.addEventListener("click",showHideButtonHandler)
+            deleteBoardButton.addEventListener("click", deleteBoardButtonHandler)
+            showHideButton.addEventListener("click", showHideButtonHandler)
             const boardTitle = document.querySelector(`.board-title[data-board-id="${board.id}"]`)
             boardTitle.addEventListener('keydown', updateTitle)
         }
@@ -27,37 +33,126 @@ async function showHideButtonHandler(clickEvent) {
     const board = clickEvent.currentTarget.parentElement.parentElement;
     const boardId = board.dataset.boardId;
     const addColumnButton = document.querySelector(`.add-column-button[data-board-id="${boardId}"]`);
-    addColumnButton.addEventListener("click",addColumnHandler);
+    addColumnButton.addEventListener("click", addColumnHandler);
     let columns = document.querySelector(`.board-columns[data-board-id="${boardId}"]`);
     let addCardButton = document.querySelector(`.board-add[data-board-id="${boardId}"]`);
-    if(board.childElementCount>1){
+    if (board.childElementCount > 1) {
         addCardButton.remove();
         columns.remove();
         addColumnButton.style.display = 'none';
-    }else {
+    } else {
         addCardButton = addCardButtonBuilder(boardId);
         columns = await columnsBuilder(boardId);
         domManager.addChild(`.board[data-board-id="${boardId}"]`, columns);
         domManager.addChild(`.board-header[data-board-id="${boardId}"]`, addCardButton);
         addCardButton = document.querySelector(`.board-add[data-board-id="${boardId}"]`);
-        addCardButton.addEventListener("click",addCardButtonHandler);
+        addCardButton.addEventListener("click", addCardButtonHandler);
         const deleteColumButtons = document.querySelectorAll(".fas.fa-trash-alt.remove-column")
-        deleteColumButtons.forEach(button => {button.addEventListener("click", deleteColumnHandler)})
+        deleteColumButtons.forEach(button => {
+            button.addEventListener("click", deleteColumnHandler)
+        })
         await cardsManager.loadCards(boardId);
         addColumnButton.style.display = 'inline';
+        initDragAndDrop();
     }
 }
 
-async function addCardButtonHandler(clickEvent){
+function initDragAndDrop() {
+    let cards = document.querySelectorAll('.card')
+    const columns = document.querySelectorAll('.board-column-content')
+    let currentDrag = null;
+    for (let card of cards) {
+        card.draggable = true
+        card.ondragstart = (e) => {
+            currentDrag = card
+            for (let card of cards) {card.classList.add('hint')}
+            // console.log(card)
+            for (let column of columns) {
+                if (column.dataset.boardId === card.dataset.boardId) {
+                    column.classList.add('dropzone-column')
+                }
+            }
+        };
+        card.ondragenter = (e) => {
+            // console.log(currentDrag)
+            // console.log(card)
+            if (currentDrag !== card && currentDrag.dataset.boardId === card.dataset.boardId) {
+                card.classList.add('active')
+            }
+        };
+        card.ondragleave = (e) => {
+            card.classList.remove('active')
+        };
+        card.ondragend = (e) => {
+            for (let column of columns) {
+                column.classList.remove('dropzone-column')
+            }
+            for (let card of cards) {
+                card.classList.remove('active')
+                card.classList.remove('hint')
+            }
+
+        };
+        card.ondragover = (e) => {
+            e.preventDefault()
+        }
+        card.ondrop = (e) => {
+            e.preventDefault();
+            if (card !== currentDrag){
+                if(card.dataset.boardId === currentDrag.dataset.boardId) {
+                    let currentColumn = currentDrag.parentElement
+                    let targetColumn = card.parentElement
+                    console.log(currentColumn)
+                    console.log(targetColumn)
+                    if (card.dataset.cardStatusId !== currentDrag.dataset.cardStatusId) {
+                        console.log('status changed, rearrange two col')
+                    } else {
+                        console.log('status remains, rearrange one col')
+                    }
+                    targetColumn.insertBefore(currentDrag,card)
+                } else {
+                    console.log('drop on card in different board is not allowed')
+                }
+            } else {
+                console.log('drop on self nothing changed')
+            }
+        }
+    }
+    for(let column of columns){
+        column.ondragover = (e) => {
+            e.preventDefault()
+        }
+        column.ondrop = (e) => {
+            if(e.target === column) {
+                if (currentDrag.dataset.boardId === column.dataset.boardId) {
+                    if(currentDrag.dataset.cardStatusId === column.dataset.statusId) {
+                        column.append(currentDrag)
+                        console.log('status not changed, insert as last')
+                    } else {
+                        column.append(currentDrag)
+                        console.log('status changed, insert as last')
+                    }
+                } else {
+                    // handle when card dropped to another board column
+                    console.log('drop on column in different board is not allowed')
+                }
+            }
+        }
+    }
+    // console.log(columns)
+    // console.log(cards)
+}
+
+async function addCardButtonHandler(clickEvent) {
     const boardId = clickEvent.target.dataset.boardId
     const newCard = await dataHandler.createNewCard("newCard", boardId);
-    domManager.addChild(`.board-column-content[data-board-id="${boardId}"]`,cardBuilder(newCard));
+    domManager.addChild(`.board-column-content[data-board-id="${boardId}"]`, cardBuilder(newCard));
     const newCardDeleteButton = document.querySelector(`[data-card-id-remove="${newCard.id}"]`)
     domManager.addEventListener(
-            `[data-card-id-remove="${newCard.id}"]`,
-            "click",
-            deleteCardButtonHandler
-            )
+        `[data-card-id-remove="${newCard.id}"]`,
+        "click",
+        deleteCardButtonHandler
+    )
 }
 
 
@@ -91,22 +186,22 @@ export function addColumnHandler(event) {
     const addColumnButton = document.querySelector('.add-column-button');
     document.querySelector('.modal.add-column-modal').classList.add('new-board-modal');
     const saveChangesButton = document.querySelector('#save-status-button')
-            saveChangesButton.addEventListener('click', saveChangesButton.handler=async () => {
-                document.querySelector('.modal.add-column-modal').classList.remove('new-board-modal');
-                const columnStatus = document.querySelector('#column-status').value;
-                if (columnStatus !== '') {
-                    const response = await dataHandler.createNewColumn(columnStatus, boardID);
-                    const newColumn = newColumnBuilder(boardID, response);
-                    saveChangesButton.removeEventListener('click', saveChangesButton.handler);
-                    domManager.addChild(`.board-columns[data-board-id="${boardID}"]`, newColumn);
-                    const deleteColumnButton = document.querySelector(`.fas.fa-trash-alt.remove-column[data-status-id-remove="${response.id}"]`);
-                    deleteColumnButton.addEventListener("click", deleteColumnHandler);
+    saveChangesButton.addEventListener('click', saveChangesButton.handler = async () => {
+        document.querySelector('.modal.add-column-modal').classList.remove('new-board-modal');
+        const columnStatus = document.querySelector('#column-status').value;
+        if (columnStatus !== '') {
+            const response = await dataHandler.createNewColumn(columnStatus, boardID);
+            const newColumn = newColumnBuilder(boardID, response);
+            saveChangesButton.removeEventListener('click', saveChangesButton.handler);
+            domManager.addChild(`.board-columns[data-board-id="${boardID}"]`, newColumn);
+            const deleteColumnButton = document.querySelector(`.fas.fa-trash-alt.remove-column[data-status-id-remove="${response.id}"]`);
+            deleteColumnButton.addEventListener("click", deleteColumnHandler);
 
         }
     });
 }
 
-function deleteColumnHandler(event){
+function deleteColumnHandler(event) {
     dataHandler.deleteStatus(event.target.dataset.statusIdRemove)
     event.target.parentElement.parentElement.remove()
 }
