3250d9cb887c4e70f5e2b5c3fe918d327061678b
Daniel Fekete <danielt.fekete@gmail.com>
2022-06-01 22:01:05 +0200

    Fix drag and drop function on backend
    -prevent drop cards to another board than parent
    -implement rearrange card position

--- static/js/controller/cardsManager.js
+++ static/js/controller/cardsManager.js
@@ -12,35 +12,35 @@ export let cardsManager = {
             const cardTitle = document.querySelector(`.card-title[data-card-id="${card.id}"]`);
             cardTitle.addEventListener('keydown', updateTitle);
         }
-        const allCards = document.querySelectorAll('.card')
-        const cols = document.querySelectorAll('.board-column-content');
-
-        allCards.forEach(card => {
-            card.setAttribute('draggable', true);
-            card.addEventListener('dragstart', () => {
-                card.classList.add('dragging');
-            });
-
-            card.addEventListener('dragend', () => {
-                    card.classList.remove('dragging')
-            });
-        });
-
-        cols.forEach(col => {
-            col.addEventListener('dragover', e => {
-                e.preventDefault();
-            });
-
-            col.addEventListener('drop', () => {
-                const card = document.querySelector('.dragging');
-                if (boardId === card.dataset.boardId) {
-                    col.append(card);
-                    const newStatusId = card.parentElement.dataset.statusId;
-                    updateCardStatus(newStatusId, card.dataset.cardId);
-                }
-                else console.log('no, not in here')
-            })
-        });
+        // const allCards = document.querySelectorAll('.card')
+        // const cols = document.querySelectorAll('.board-column-content');
+        //
+        // allCards.forEach(card => {
+        //     card.setAttribute('draggable', true);
+        //     card.addEventListener('dragstart', () => {
+        //         card.classList.add('dragging');
+        //     });
+        //
+        //     card.addEventListener('dragend', () => {
+        //             card.classList.remove('dragging')
+        //     });
+        // });
+        //
+        // cols.forEach(col => {
+        //     col.addEventListener('dragover', e => {
+        //         e.preventDefault();
+        //     });
+        //
+        //     col.addEventListener('drop', () => {
+        //         const card = document.querySelector('.dragging');
+        //         if (boardId === card.dataset.boardId) {
+        //             col.append(card);
+        //             const newStatusId = card.parentElement.dataset.statusId;
+        //             updateCardStatus(newStatusId, card.dataset.cardId);
+        //         }
+        //         else console.log('no, not in here')
+        //     })
+        // });
     }
 };
 
